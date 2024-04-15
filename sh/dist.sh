445531ce3b1bdc47a0e8347495dcf0c4510dbeb4
i18n <i18n.site@gmail.com>
2024-04-15 15:21:06 +0800

    Add a script deployed as systemctl 添加部署为 systemctl 的脚本

--- sh/dist.sh
+++ sh/dist.sh
@@ -18,6 +18,22 @@ if ! [[ $msg =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
     git merge $branch
   fi
   git push
+  msg=$(git log -1 --pretty=format:'%B' $branch)
+  git push github $msg
 fi
 
-./setup.sh
+# ./setup.sh
+#
+# CONF=$(realpath $DIR/../../conf)
+# . $CONF/env/dist_to.sh
+#
+# NAME=$(cargo metadata --format-version=1 --no-deps | jq -r '.packages[0].name')
+# FILE_LI="/opt/bin/$NAME.sh /opt/bin/$NAME /etc/systemd/system/$NAME.service"
+#
+# for srv in $DIST_TO; do
+#   for file in $FILE_LI; do
+#     rsync --mkpath -avz $file $srv:$file &
+#   done
+#   wait
+#   ssh $srv "set -ex && cd $CONF && git pull && systemctl daemon-reload && systemctl enable --now $NAME && systemctl restart $NAME"
+# done
