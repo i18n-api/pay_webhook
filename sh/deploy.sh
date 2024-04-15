24df191c5ba6dc786c6d31bfc471d48947a8d19a
i18n <i18n.site@gmail.com>
2024-04-16 01:01:10 +0800

    .

--- sh/deploy.sh
+++ sh/deploy.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bash
 
+set -e
+
 DIR=$(realpath $0) && DIR=${DIR%/*}
 cd $DIR
-set -ex
+
+if [ -z "$1" ]; then
+  echo "USAGE : $0 project_name"
+  exit 1
+else
+  conf=$1
+  . $conf/api_srv.sh
+  key=$conf/ssh/id_ed25519
+  chmod 600 $key
+fi
+
+set -x
 
 VER=$(cargo metadata --format-version=1 --no-deps | jq -r '.packages[0].version')
 
@@ -12,8 +25,8 @@ REPO=${REPO%.git}
 
 URL=https://raw.githubusercontent.com/i18n-ops/ops/main/curl.setup.sh
 
-ssh="sshpass -e ssh -q -o StrictHostKeyChecking=no"
+ssh="ssh -q -o StrictHostKeyChecking=no -F $conf/ssh_config -i $key"
 
 for srv in $API_SRV; do
-  $ssh $srv "curl -sSf $URL|bash -s -- $REPO $VER"
+  $ssh root@$srv "curl -sSf $URL|bash -s -- $REPO $VER"
 done
