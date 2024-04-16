e2f46a3392b8e99cebac94aa7f88eb8f272c4ecc
i18n <i18n.site@gmail.com>
2024-04-16 14:34:49 +0800

    Adjust the parameters of./sh/deploy.sh 调整 ./sh/deploy.sh 的参数

--- sh/deploy.sh
+++ sh/deploy.sh
@@ -5,4 +5,6 @@ set -e
 DIR=$(realpath $0) && DIR=${DIR%/*}
 cd $DIR
 
-curl -sSf https://raw.githubusercontent.com/i18n-ops/ops/main/setup/deploy.sh | bash -s -- $@
+META=$(cargo metadata --format-version=1 --no-deps | jq '.packages[0] | .name + ":" + .version' -r)
+
+curl -sSf https://raw.githubusercontent.com/i18n-ops/ops/main/setup/deploy.sh | bash -s -- $META $@
