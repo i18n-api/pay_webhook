9331bb47e00a70d75f54763814a1cea7966e639d
i18n <i18n.site@gmail.com>
2024-11-28 21:05:46 +0800

    ♨

--- sh/github.dist.sh
+++ sh/github.dist.sh
@@ -42,43 +42,4 @@ mkdir -p $sys
 
 cp $NAME.service $sys/
 
-case "$(uname -s)" in
-"Darwin")
-  OS="apple-darwin"
-  ;;
-"Linux")
-  (ldd --version 2>&1 | grep -q musl) && clib=musl || clib=gun
-  OS="unknown-linux-$clib"
-  ;;
-"MINGW*" | "CYGWIN*")
-  OS="pc-windows-msvc"
-  ;;
-*)
-  echo "Unsupported System"
-  exit 1
-  ;;
-esac
-
-ARCH=$(uname -m)
-
-if [[ "$ARCH" == "arm64" || "$ARCH" == "arm" ]]; then
-  ARCH="aarch64"
-fi
-
-TZT=$ARCH-$OS.tar.zst
-
-cd $DIROS
-ZSTD_CLEVEL=19 tar --owner=root --group=root -I zstd -cvpf ../$TZT .
-cd ..
-
-LOG=$ROOT/log/$VER.md
-
-if [ -f "$LOG" ]; then
-  NOTE="-F $LOG"
-else
-  NOTE="-n $VER"
-fi
-
-gh release create -d $VER $NOTE
-gh release upload $VER $TZT
-gh release edit $VER --draft=false
+$ROOT/../dist/encrypt.sh $DIROS
