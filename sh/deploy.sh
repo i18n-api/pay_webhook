6c3b21afdd3f1877634e4236da2604d0a6c3273c
i18n <i18n.site@gmail.com>
2024-11-28 16:17:48 +0800

    ♨

--- sh/deploy.sh
+++ sh/deploy.sh
@@ -7,4 +7,4 @@ cd $DIR
 
 ver=$(cargo metadata --format-version=1 --no-deps | jq '.packages[0] | .version' -r)
 
-sudo bash <(curl -sSL https://raw.githubusercontent.com/i18n-ops/ops/refs/heads/main/srv/deploy.sh) pay_webhook $ver
+curl -sSL https://raw.githubusercontent.com/i18n-ops/ops/refs/heads/main/srv/deploy.sh | sudo bash -s pay_webhook $ver
